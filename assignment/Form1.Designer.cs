2c4b8aa13b815de89eec09aee5257f0adac31e30
ShisanuchaTham <153785521+ShisanuchaTham@users.noreply.github.com>
2024-12-06 17:43:08 +0700

    Add files via upload

---
+++ assignment/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace Carmara
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox_time = new System.Windows.Forms.TextBox();
+            this.textBox_Record = new System.Windows.Forms.TextBox();
+            this.textBox_Connection = new System.Windows.Forms.TextBox();
+            this.textBox_date = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.imageBox1);
+            this.groupBox1.Location = new System.Drawing.Point(33, 22);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(745, 303);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "User";
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.Location = new System.Drawing.Point(16, 22);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(723, 275);
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.textBox_time);
+            this.groupBox2.Controls.Add(this.textBox_Record);
+            this.groupBox2.Controls.Add(this.textBox_Connection);
+            this.groupBox2.Controls.Add(this.textBox_date);
+            this.groupBox2.Location = new System.Drawing.Point(33, 331);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(745, 107);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Command";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(171, 31);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(133, 52);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Record";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 31);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 52);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox_time
+            // 
+            this.textBox_time.Location = new System.Drawing.Point(639, 47);
+            this.textBox_time.Name = "textBox_time";
+            this.textBox_time.Size = new System.Drawing.Size(100, 22);
+            this.textBox_time.TabIndex = 3;
+            this.textBox_time.Text = "time";
+            // 
+            // textBox_Record
+            // 
+            this.textBox_Record.Location = new System.Drawing.Point(533, 72);
+            this.textBox_Record.Name = "textBox_Record";
+            this.textBox_Record.Size = new System.Drawing.Size(100, 22);
+            this.textBox_Record.TabIndex = 2;
+            // 
+            // textBox_Connection
+            // 
+            this.textBox_Connection.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.textBox_Connection.Location = new System.Drawing.Point(427, 72);
+            this.textBox_Connection.Name = "textBox_Connection";
+            this.textBox_Connection.Size = new System.Drawing.Size(100, 22);
+            this.textBox_Connection.TabIndex = 1;
+            // 
+            // textBox_date
+            // 
+            this.textBox_date.Location = new System.Drawing.Point(639, 72);
+            this.textBox_date.Name = "textBox_date";
+            this.textBox_date.Size = new System.Drawing.Size(100, 22);
+            this.textBox_date.TabIndex = 0;
+            this.textBox_date.Text = "date";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox_time;
+        private System.Windows.Forms.TextBox textBox_Record;
+        private System.Windows.Forms.TextBox textBox_Connection;
+        private System.Windows.Forms.TextBox textBox_date;
+    }
+}
+
---
+++ assignment/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Emgu.CV;
+
+namespace Carmara
+{
+
+    public partial class Form1 : Form
+    {
+        private VideoCapture _capture = null;
+        private Mat _flame;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            textBox_Connection.BackColor = Color.Red;
+            textBox_Connection.Text = "Disconnected";
+            button2.Enabled = false;
+            textBox_Record.BackColor = Color.Red;
+            textBox_Record.Text = "Not recording";
+        }
+
+        private void ProcessFrame(object sender, EventArgs e)
+        {
+            if (_capture != null && _capture.Ptr != IntPtr.Zero)
+            {
+                bool canCapture = _capture.Retrieve(_flame, 0);
+                if (canCapture)
+                {
+                    imageBox1.Image = _flame;
+                }
+            }
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                textBox_Connection.BackColor = Color.Blue;
+                textBox_Connection.Text = "Waiting";
+                await Task.Delay(1);
+
+                if (button1.Text == "Connect")
+                {
+                    _capture = new VideoCapture();
+                    _capture.ImageGrabbed += ProcessFrame;
+
+                    _flame = new Mat();
+
+                    textBox_Connection.BackColor = Color.Green;
+                    textBox_Connection.Text = "Connected";
+
+                    button1.Text = "Disconnect";
+
+                    button2.Enabled = true;
+
+                }
+                else if (button1.Text == "Disconnect")
+                {
+                    if (_capture != null)
+                    {
+                        _capture.Pause();
+                        _capture.Dispose();
+                    }
+                    if (_flame != null)
+                    {
+                        _flame.Dispose();
+                    }
+
+                    textBox_Connection.BackColor = Color.Red;
+                    textBox_Connection.Text = "Disconnected";
+
+                    button1.Text = "Connect";
+
+                    button2.Enabled = false;
+
+
+                }
+
+            }
+            catch (NullReferenceException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private bool recording = false;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (_capture != null)
+            {
+                if (!recording)
+                {
+
+                    _capture.Start();
+                    recording = true;
+
+                    button1.Enabled = false;
+
+                    button2.Text = "Pause";
+
+                    textBox_Record.BackColor = Color.Green;
+                    textBox_Record.Text = "Recording";
+
+                }
+                else
+                {
+
+                    _capture.Pause();
+                    recording = false;
+
+                    button1.Enabled = true;
+
+                    button2.Text = "Record";
+
+                    textBox_Record.BackColor = Color.Red;
+                    textBox_Record.Text = "Not recording";
+
+                }
+            }
+        }
+    }
+}
